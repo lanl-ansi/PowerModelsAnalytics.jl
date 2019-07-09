3dbed5cc3146c709021a743e62b56c48906a8ae4
David M Fobes <dfobes@lanl.gov>
2019-07-09 09:42:17 -0600

    ADD: export.jl for autoexport

--- src/PowerModelsAnalytics.jl
+++ src/PowerModelsAnalytics.jl
@@ -1,43 +1,45 @@
 module PowerModelsAnalytics
 
-import InfrastructureModels
-import PowerModels
-import Plots
-import Memento
+    import InfrastructureModels
+    import PowerModels
+    import Plots
+    import Memento
 
-import MetaGraphs
-import GraphPlot
+    import MetaGraphs
+    import GraphPlot
 
-import Colors
-import Colors: @colorant_str
-import ColorVectorSpace
-import FixedPointNumbers
+    import Colors
+    import Colors: @colorant_str
+    import ColorVectorSpace
+    import FixedPointNumbers
 
-import Compose
+    import Compose
 
-import Statistics: mean, std
-import Random: rand
+    import Statistics: mean, std
+    import Random: rand
 
-import PyCall
+    import PyCall
 
-# Create our module level logger
-const LOGGER = Memento.getlogger(@__MODULE__)
+    # Create our module level logger
+    const LOGGER = Memento.getlogger(@__MODULE__)
 
-const nx = PyCall.PyNULL()
-const scipy = PyCall.PyNULL()
+    const nx = PyCall.PyNULL()
+    const scipy = PyCall.PyNULL()
 
-function __init__()
-    Memento.register(LOGGER)
+    function __init__()
+        Memento.register(LOGGER)
 
-    copy!(nx, PyCall.pyimport_conda("networkx", "networkx"))
-    copy!(scipy, PyCall.pyimport_conda("scipy", "scipy"))
-end
+        copy!(nx, PyCall.pyimport_conda("networkx", "networkx"))
+        copy!(scipy, PyCall.pyimport_conda("scipy", "scipy"))
+    end
 
-include("core/layouts.jl")
-include("core/parameters.jl")
-include("core/plots.jl")
+    include("core/layouts.jl")
+    include("core/parameters.jl")
+    include("core/plots.jl")
 
-include("graph/metrics.jl")
+    include("graph/metrics.jl")
+
+    include("core/export.jl")
 
 end
 
