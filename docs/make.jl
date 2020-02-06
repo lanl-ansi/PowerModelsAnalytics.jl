d4e66f391265ff52f9e0c0338e87ed67d7b5d068
David M Fobes <dfobes@lanl.gov>
2020-02-06 15:59:05 -0700

    UPD: mathengine for Documenter

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using Documenter, PowerModelsAnalytics
 
 makedocs(
     modules = [PowerModelsAnalytics],
-    format = Documenter.HTML(analytics = ""),
+    format = Documenter.HTML(analytics = "", mathengine = Documenter.MathJax()),
     sitename = "PowerModelsAnalytics",
     authors = "David M Fobes, Carleton Coffrin, and contributors.",
     pages = [
